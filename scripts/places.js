3d79e2253d5ee88ba37afbe63f58d96132d9485b
anton <anton004@protonmail.com>
2025-03-22 17:57:57 +0200

    lr 2

---
+++ scripts/places.js
@@ -0,0 +1,10 @@
+let places = ["Кабул", "Єрусалим", "Краків", "Париж"]
+let i = 0
+// do {
+//     console.log(places[i])
+//     i++
+// } while (i < places.length)
+
+for (i in places) {
+    console.log(places[i])
+}
